24dc83a41de3e4ded86870dcf9f7ddb117762017
khairnaswapna <khairnaswapna99@gmail.com>
2019-11-15 12:01:36 +0530

    If player reaches position 100 then win and if position lt 0 then position is 0

--- SnakeLadderStimulator.sh
+++ SnakeLadderStimulator.sh
@@ -24,21 +24,30 @@ function rollDice()
 }
 
 echo "ROLL THE DICE"
-moveOption=$(playOption)
-moveValue=$(rollDice)
+while [ $position -le 100 ]
+do
+	moveOption=$(playOption)
+	moveValue=$(rollDice)
 
-if [[  $moveOption -eq $NOPLAY ]]
-then
-	echo "NO MOVE"
-	echo "POSITION:" $position
-elif [[ $moveOption -eq $LADDER ]]
-then
-	echo "YOU WILL MOVE BY POSITION" $moveValue
-	position=$(( $moveValue + $position ))
-	echo "POSITION:"$position
-elif [[ $moveOption -eq $SNAKE ]]
-then
-	echo "YOU WILL COME BEHIND BY POSITION" $moveValue
-	position=$(( $position - $moveValue ))
-	echo "POSITION:" $position
-fi
+	if [[  $moveOption -eq $NOPLAY ]]
+	then
+		echo "NO MOVE"
+		echo "POSITION:" $position
+	elif [[ $moveOption -eq $LADDER ]]
+	then
+		echo "YOU WILL MOVE BY POSITION" $moveValue
+		position=$(( $moveValue + $position ))
+		echo "POSITION:"$position
+	elif [[ $moveOption -eq $SNAKE ]]
+	then
+		echo "YOU WILL COME BEHIND BY POSITION" $moveValue
+		position=$(( $position - $moveValue ))
+		echo "POSITION:" $position
+	fi
+	if [ $position -lt 0 ]
+	then
+		position=0
+	fi
+done
+echo "YOU WIN"
+echo "FINAL POSITION IS: " $position
