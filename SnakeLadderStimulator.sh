fa845aeeb564a0aa16eecdc897c37700778d2332
khairnaswapna <khairnaswapna99@gmail.com>
2019-11-15 11:49:00 +0530

    Move the player according to the options

--- SnakeLadderStimulator.sh
+++ SnakeLadderStimulator.sh
@@ -1,9 +1,44 @@
-#!/bin/bash -x
+#!/bin/bash -x 
 
 echo "WELCOME TO SNAKE AND LADDER STIMULATOR"
 
 #VARIABLES
 position=0
+NOPLAY=0
+LADDER=1
+SNAKE=2
+moveValue=0
+moveOption=0
 
-diceRoll=$[ ($RANDOM%6) +1 ]
-echo $diceRoll
+
+function playOption()
+{
+	option=$[ ($RANDOM%3) ]
+	echo $option
+}
+
+function rollDice()
+{
+	diceRoll=$[ ($RANDOM%6) +1 ]
+	echo $diceRoll
+}
+
+echo "ROLL THE DICE"
+moveOption=$(playOption)
+moveValue=$(rollDice)
+
+if [[  $moveOption -eq $NOPLAY ]]
+then
+	echo "NO MOVE"
+	echo "POSITION:" $position
+elif [[ $moveOption -eq $LADDER ]]
+then
+	echo "YOU WILL MOVE BY POSITION" $moveValue
+	position=$(( $moveValue + $position ))
+	echo "POSITION:"$position
+elif [[ $moveOption -eq $SNAKE ]]
+then
+	echo "YOU WILL COME BEHIND BY POSITION" $moveValue
+	position=$(( $position - $moveValue ))
+	echo "POSITION:" $position
+fi
